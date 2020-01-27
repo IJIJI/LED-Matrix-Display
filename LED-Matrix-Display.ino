448b61037fbb322f1fc01cbae0e5e0bed62879ec
IJIJI <ijsslkstr@gmail.com>
2020-01-27 18:20:24 +0100

    Update LED-Matrix-Display.ino

---
+++ LED-Matrix-Display.ino
@@ -0,0 +1,178 @@
+
+#include "Arduino.h"
+#include "LedControl.h"
+
+#define dispAmount 0 // amount of displays -1
+#define switchPin 2
+
+LedControl lc=LedControl(7,5,6, dispAmount);  // Pins: DIN,CLK,CS, # of Display connected
+
+int delayTime=200;  // Delay between Frames
+
+
+byte frames[] =
+{
+  B10000000,
+  B01000000,
+  B00100000,
+  B00010000,
+  B00001000,
+  B00000100,
+  // B00000010,
+  // B00000001,
+  B00000000
+};
+
+byte sideFillFrames[]{
+  B10000000,
+  B11000000,
+  B11100000,
+  B11110000,
+  B11111000,
+  B11111100,
+  // B11111110,
+  // B11111111,
+  B01111111,
+  B00111111,
+  B00011111,
+  B00001111,
+  B00000111,
+  // B00000011,
+  // B00000001,
+  B00000000
+};
+
+ byte a[8]={0x30, 0x78, 0xcc, 0xcc, 0xfc, 0xcc, 0xcc, 0x00};    
+ byte b[8]={0xfc, 0x66, 0x66, 0x7c, 0x66, 0x66, 0xfc, 0x00};  
+ byte c[8]={0x3c, 0x66, 0xc0, 0xc0, 0xc0, 0x66, 0x3c, 0x00};  
+ byte d[8]={0xf8, 0x6c, 0x66, 0x66, 0x66, 0x6c, 0xf8, 0x00};  
+ byte e[8]={0xfe, 0x62, 0x68, 0x78, 0x68, 0x62, 0xfe, 0x00};
+ byte f[8]={0xfe, 0x62, 0x68, 0x78, 0x68, 0x60, 0xf0, 0x00};
+ byte g[8]={0x3c, 0x66, 0xc0, 0xc0, 0xce, 0x66, 0x3e, 0x00};
+
+
+void setup()
+{
+  for(int x = 0; x < dispAmount + 1; x++){
+    
+    lc.shutdown(x,false);  // Wake up displays
+    lc.setIntensity(x,5);  // Set intensity levels
+    lc.clearDisplay(x);  // Clear Displays
+    
+    
+  }
+  
+  pinMode(switchPin, INPUT_PULLUP);
+}
+
+
+void ledCycle(){
+  for (int x = 0; x < 8; x++){
+    for (int y = 0; y < sizeof(frames); y++){
+      
+      for (int z = 0; z < dispAmount + 1; z++){
+        lc.setRow(z,x,frames[y]);
+      }
+      
+      delay(delayTime);
+    }
+  }
+}
+
+void sideFill(){
+  for (int y = 0; y < sizeof(sideFillFrames); y++){
+    for (int x = 0; x < 8; x++){
+      
+      for (int z = 0; z < dispAmount + 1; z++){
+        lc.setRow(z,x,sideFillFrames[y]);
+      }
+    }
+    delay(delayTime * 2);
+  }
+  
+}
+
+void fill(){
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      
+      lc.setRow(z,x,255);
+    }
+    delay(delayTime * 2);
+  }
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,0);
+    }
+    delay(delayTime * 2);
+  }
+}
+void white(){
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,255);
+    }
+  }
+}
+
+
+void alphabet(){
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,a[x]);
+    }
+  }
+  delay(delayTime * 8);
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,b[x]);
+    }
+  }
+  delay(delayTime * 8);
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,c[x]);
+    }
+  }
+  delay(delayTime * 8);
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,d[x]);
+    }
+  }
+  delay(delayTime * 8);
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,e[x]);
+    }
+  }
+  delay(delayTime * 8);
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,f[x]);
+    }
+  }
+  delay(delayTime * 8);
+  for (int x = 0; x < 8; x++){
+    for (int z = 0; z < dispAmount + 1; z++){
+      lc.setRow(z,x,g[x]);
+    }
+  }
+  delay(delayTime * 8);
+  for (int z = 0; z < dispAmount + 1; z++){
+    lc.clearDisplay(z); 
+  }
+}
+
+void loop()
+{
+  if (digitalRead(switchPin) == HIGH){
+    ledCycle();
+    fill();
+    sideFill();
+  }
+  else{
+    alphabet();
+    white();
+  }
+}
