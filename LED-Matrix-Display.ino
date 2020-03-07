9222e4a15d68e81dda812b841da2b8abfdd15783
IJIJI <ijsslkstr@gmail.com>
2020-03-07 14:24:15 +0100

    overhauled... everything

--- LED-Matrix-Display.ino
+++ LED-Matrix-Display.ino
@@ -1,62 +1,41 @@
 
 #include "Arduino.h"
-#include "LedControl.h"
-// #include "matrixDisplay.h"
-#include "font.h"
-
-#define thin
 
 
 #define dispAmount 30 // amount of displays
-#define switchPin 2
-
-
-
-LedControl lc=LedControl(11,13,8, dispAmount - 1);  // Pins: DIN,CLK,CS, # of Display connected
-
-#define delayTime 600  // Delay between Frames
+#define switch1 6
+#define switch2 5
+#define switch3 4
 
 
 
 
 void setup()
 {
-  for(int x = 0; x < dispAmount; x++){
-    
-    lc.shutdown(x,false);  // Wake up displays
-    lc.setIntensity(x,15);  // Set intensity levels
-    lc.clearDisplay(x);  // Clear Displays
-    
-    
-  }
   Serial.begin(9600);
-  pinMode(switchPin, INPUT_PULLUP);
+  pinMode(switch1, INPUT_PULLUP);
+  pinMode(switch2, INPUT_PULLUP);
+  pinMode(switch3, INPUT_PULLUP);
 }
 
 
-// different swipe animations vv
+// TODO improve to byte instead of int ?
 
-void white(){
-  for (int x = 0; x < 8; x++){
-    for (int z = 0; z < dispAmount; z++){
-      lc.setRow(z,x,255);
-    }
+int readSwitches(){
+  int mode = 0;
+  if (digitalRead(switch1) == LOW)
+  {
+    mode += 1;
   }
-  delay(delayTime * 8);
-}
-
-
-// ? Improved Alphabet()
-
-void fontCycle(){
-  for (int i = 0; i < fontLength; i++){
-    for (int x = 0; x < 8; x++){
-      for (int z = 0; z < dispAmount; z++){
-        lc.setRow(z,x,font[i][x]);
-      }
-    }
-    delay(delayTime * 4);
+  if (digitalRead(switch2) == LOW)
+  {
+    mode += 2;
   }
+  if (digitalRead(switch3) == LOW){
+    mode += 4;
+  }
+  
+  
 }
 
 
@@ -64,28 +43,5 @@ void fontCycle(){
 
 void loop()
 {
-  if (digitalRead(switchPin) == HIGH){
-    fontCycle();
-    white();
-  }
-  else{
-    
-    char inData = ' ';
-    while (Serial.available() > 0) {
-      inData = char(Serial.read());
-      delay(5);
-      Serial.println(inData);
 
-      for (int y = 0; y < fontLength; y++){
-        if (inData == fontContent[y]){
-          for (int x = 0; x < 8; x++){
-            for (int z = 0; z < dispAmount; z++){
-              lc.setRow(z,x,font[y][x]);
-            }
-          }
-          delay(delayTime);
-        }
-      }
-    }
-  }
 }
