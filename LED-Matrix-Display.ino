1985f3a246898bfdc30644b9e82f2625d0b315ef
IJIJI <ijsslkstr@gmail.com>
2020-03-08 13:37:27 +0100

    implemented simple serial print

--- LED-Matrix-Display.ino
+++ LED-Matrix-Display.ino
@@ -1,14 +1,29 @@
 
 #include "Arduino.h"
 
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
 
 #define dispAmount 30 // amount of displays
+
 #define switch1 6
 #define switch2 5
 #define switch3 4
 
+#define CLK_PIN   13
+#define DATA_PIN  11
+#define CS_PIN    8
 
+// #define HARDWARE_TYPE MD_MAX72XX::PAROLA_HW
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW 
 
+// Hardware SPI connection
+MD_Parola matrix = MD_Parola(HARDWARE_TYPE, CS_PIN, dispAmount);
+// Arbitrary output pins
+// MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+String inData;
 
 void setup()
 {
@@ -16,6 +31,11 @@ void setup()
   pinMode(switch1, INPUT_PULLUP);
   pinMode(switch2, INPUT_PULLUP);
   pinMode(switch3, INPUT_PULLUP);
+
+  matrix.begin();
+
+
+  matrix.setTextAlignment(PA_CENTER);
 }
 
 
@@ -39,9 +59,24 @@ int readSwitches(){
 }
 
 
+
+
 // Cycle through functions VV
 
 void loop()
 {
 
+  if (Serial.available()){
+    inData = "";
+    while (Serial.available() > 0) {
+      inData += char(Serial.read());
+      delay(5);
+    
+    }
+    Serial.println();
+    Serial.println(inData);
+
+    // matrix.displayText(inData, PA_CENTER, 0, 0, PA_PRINT, PA_NO_EFFECT);
+    matrix.print(inData);
+  }
 }
